de39e0d46b8d1abe0b7d81c0d13804a40717e981
Ross Paffett <ross@rosspaffett.com>
2012-02-02 18:22:03 -0500

    Track the number of active connections

--- lib/hector/ext/connection.rb
+++ lib/hector/ext/connection.rb
@@ -1,5 +1,19 @@
 module Hector
   class Connection
+    def post_init_with_statsd(*args)
+      Hector.statsd.increment("connections") if Hector.statsd
+      post_init_without_statsd(*args)
+    end
+    alias_method :post_init_without_statsd, :post_init
+    alias_method :post_init, :post_init_with_statsd
+    
+    def unbind_with_statsd(*args)
+      Hector.statsd.decrement("connections") if Hector.statsd
+      unbind_without_statsd(*args)
+    end
+    alias_method :unbind_without_statsd, :unbind
+    alias_method :unbind, :unbind_with_statsd
+    
     def receive_line_with_statsd(*args)
       Hector.statsd.increment("traffic.in") if Hector.statsd
       receive_line_without_statsd(*args)
